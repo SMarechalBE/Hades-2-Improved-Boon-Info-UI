55b969e683bd1042b3879f0119dbf58412ab47ef
SMarechalBE <samuelmarechal94@gmail.com>
2025-12-14 02:33:03 +0100

    refactor: remove public API access

--- src/def.lua
+++ src/def.lua
@@ -1,22 +1,4 @@
 ---@meta SMarBe-Improved_Boon_Info_UI
 local public = {}
 
----Get the state of the given boon in the following order of importance:<br>
---- 1. Picked<br>
---- 2. Denied (vow of denials)<br>
---- 3. Slot unavailable<br>
---- 4. God unavailable<br>
---- 5. State from its requirements, see GetBoonRequirementState
----@param traitName string
----@return BoonState
-function public.GetBoonState(traitName) end
-
----Checks whether the god is one of the Olympian (slot) boon giver
----@param godName string
----@return boolean
-function public.IsSlotGiver(godName) end
-
-
-public.BoonState = ...
-
 return public
--- src/ready.lua
+++ src/ready.lua
@@ -1,4 +1,4 @@
----@meta SMarBe-Improved_Boon_Info_UI
+---@meta _
 -- globals we define are private to our plugin!
 ---@diagnostic disable: lowercase-global
 
