8b69397faf371f4efa61bb939ba7df15ccd64886
SMarechalBE <samuelmarechal94@gmail.com>
2025-12-30 14:57:53 +0100

    docs: update readme

--- src/config.lua
+++ src/config.lua
@@ -7,7 +7,7 @@ local config = {
 
 local configDesc = {
   enabled = "Whether the mod is enabled or not",
-  sacrificeBoonsAlwaysAsAvailable = "Set to true to always consider sacrifice/replacement boons as available.",
+  sacrificeBoonsAlwaysAsAvailable = "Set to true to always consider sacrifice/replacement boons as available. This can be useful if you often use vow of denial as those will appear more often.",
   enablePinnedBoonsIconInRequirements = "Set to true to display pin icon next to pinned/tracked boons in the requirements list.",
   enableBannedBoonsIconInRequirements = "Set to true to display locked icon next to banned boons in the requirements list.",
 }
